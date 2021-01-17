284536ef1092f186bb016d8783c402c3f319b1bb
Palash Sharma <palashsharma891@gmail.com>
2021-01-17 11:18:00 +0530

    Create Implement strStr().py

---
+++ Top Interview Questions - Easy/2. Strings/Implement strStr().py
@@ -0,0 +1,10 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        len1 = len(haystack)
+        len2 = len(needle)
+        if len1 < len2:
+            return -1
+        for i in range(len1-len2+1):
+            if haystack[i:i+len2] == needle:
+                return i
+        return -1
