cc1c890479a2ab5866c27a566ac13965fb236e32
Palash Sharma <palashsharma891@gmail.com>
2024-01-15 08:47:34 +0530

    Create Merge Sorted Array.py

---
+++ 0. Arrays 101/2. Inserting Items Into an Array/Merge Sorted Array.py
@@ -0,0 +1,31 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while n > 0:
+            if m > 0 and nums1[m-1] > nums2[n-1]: #either check m here or run another while loop later if m checked earlier
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+
+# OR
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        while n > 0:
+            nums1[m+n-1] = nums2[n-1]
+            n -= 1
+                
